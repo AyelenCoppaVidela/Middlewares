b4df58391fc1f208677215c18bb504a16935d444
Tincho-91 <martin_baryuk@outlook.com>
2024-01-07 21:13:07 -0300

    corrijo detalle del middleware de guardar URL

--- src/middlewares/userLogs.js
+++ src/middlewares/userLogs.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require('path');
 
 function logMiddleware(req, res, next) {
-    fs.appendFileSync(path.join(__dirname, "../logs/userLogs.txt"),"El usuario ingresó a la ruta: " + req.url);
+    fs.appendFileSync(path.join(__dirname, "../logs/userLogs.txt"),`El usuario ingresó a la ruta: ${req.url} \n`);
 
     next();
 }
